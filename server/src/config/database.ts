74d296571ba90f801e913a8c79ae5acc091acae2
ashish-1303 <aashishmehbubani2@gmail.com>
2026-06-25 14:38:26 +0530

    Resolved some issues

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -4,5 +4,5 @@ import { env } from "./env.js";
 export const connectDatabase = async () => {
   mongoose.set("strictQuery", true);
   await mongoose.connect(env.MONGODB_URI);
-  console.log("MongoDB connected");
+  console.log("MongoDB connected successfully!");
 };
